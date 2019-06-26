2687dab224fe5f17cd096260e19f761091188200
bashbashbash1 <bashbashbash1@github.com>
2019-06-26 14:34:54 -0500

    addedfiles

--- secretfiles.py
+++ secretfiles.py
@@ -1,16 +1,22 @@
-#You need to find the secret username and password in order to access these secret files!
+#You need to find the secret username and password in order to access these secret
 
-print("You need to vim this file and edit the code in order for it to execute"
+print("You need to vim this file and edit the code in order for it to execute")
 username = input("username: ")
 password = input("password: ")
 
 
 
+#uncomment the lines below to execute the commad
+if username == username1 and password == password1:
+    import subprocess
+    import os
 
-#uncomment the lines below to execute the command
-#if username == '3557558' and password == '4792836061':
-#  from secretusername import secretpassword2
+    os.system('"C:/Users/jlvel/Desktop/username.txt"')
+
+
+# from secretusername import secretpassword2
 #   print(secretpassword2)
-#else:
-#   print("Unsuccesful login")
+else:
+   print("Unsuccesful login")
+
 
